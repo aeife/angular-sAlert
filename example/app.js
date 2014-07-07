9abc22e692710dc400df74630111dd93875845a6
aeife <andre.eife@gmail.com>
2014-07-07 08:47:10 +0200

    adjusted example

--- example/app.js
+++ example/app.js
@@ -1,14 +1,26 @@
 angular.module('myApp', ['alert'])
     .controller('mainCtrl', function ($scope, alert, $q, $timeout) {
-        alert.info('sAlert info');
-        alert.error('sAlert error');
-        alert.success('sAlert success');
-        alert.info('sAlert autoRemove').autoRemove();
+        // alert.info('sAlert info');
+        // alert.error('sAlert error');
+        // alert.success('sAlert success');
+        // alert.info('sAlert autoRemove').autoRemove();
 
-        var deferred = $q.defer();
-        alert.info('sAlert remove on promise').removeOnResolve(deferred.promise);
+        // var deferred = $q.defer();
+        // alert.info('sAlert remove on promise').removeOnResolve(deferred.promise);
 
-        $timeout(function () {
-            deferred.resolve();
-        }, 3000);
+        // $timeout(function () {
+        //     deferred.resolve();
+        // }, 3000);
+
+        $scope.addSuccess = function () {
+            alert.success('sAlert success');
+        }
+
+        $scope.addInfo = function () {
+            alert.info('sAlert info');
+        }
+
+        $scope.addError = function () {
+            alert.error('sAlert error');
+        }
     });
