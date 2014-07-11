43e9ecb01d0f336ca149f2f70977dc93cbd6592e
aeife <andre.eife@gmail.com>
2014-07-11 10:19:13 +0200

    jshint

--- src/sAlert.spec.js
+++ src/sAlert.spec.js
@@ -165,7 +165,7 @@ describe('sAlert', function () {
         });
 
         it('should set defined instance', function () {
-            sAlert.info('generic info', "myTestInstance");
+            sAlert.info('generic info', 'myTestInstance');
 
             expect(sAlert.get()[0].instance).toBe('myTestInstance');
         });
